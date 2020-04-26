148eb8014e0d49c4d4c00409952d3c63b6553e30
moddyz <moddy3d@gmail.com>
2020-04-26 18:13:04 +1200

    Added structured bindings experiment.

---
+++ experiments/structuredBindings/main.cpp
@@ -0,0 +1,29 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+// Structured bindings in C++17.
+//
+// Initialize multiple variables from a tuple, pair, or struct concisely.
+
+using Vec3f = std::tuple< float, float, float >;
+
+Vec3f GetZeroVector()
+{
+    return Vec3f{0.0f, 0.0f, 0.0f};
+}
+
+TEST_CASE( "structuredBindings" )
+{
+    // De-composition of tuples.
+    const auto [ x, y, z ] = GetZeroVector();
+    CHECK( x == 0.0f );
+    CHECK( y == 0.0f );
+    CHECK( z == 0.0f );
+
+    // De-composition of map items.
+    std::unordered_map< std::string, int > mapping{{"a", 1}, {"b", 2}, {"c", 3}};
+    for ( const auto& [ key, value ] : mapping )
+    {
+        printf( "%s: %d\n", key.c_str(), value );
+    }
+}
