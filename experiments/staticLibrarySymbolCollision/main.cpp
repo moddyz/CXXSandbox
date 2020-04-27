0e8f8698daa97eae613817ab3124c4f5cb70185e
moddyz <moddy3d@gmail.com>
2020-04-27 22:24:41 +1200

    Updated comment about link order in determining load order (is that always true?)

--- experiments/staticLibrarySymbolCollision/main.cpp
+++ experiments/staticLibrarySymbolCollision/main.cpp
@@ -14,10 +14,15 @@ std::string FindLibrary();
 // sharedLibA links staticLibA.
 // sharedLibB links staticLibB.
 //
+// This test executable links sharedLibA then sharedLibB, in that order.
+//
 // Each static library defines the GetLibraryName() symbol, which sharedLibA and sharedLibB call, respectively,
 // in their FindLibrary implementations.
 //
-// B::FindLibrary() would return "staticLibB" if this test program does _not_ link sharedLibA.
+// B::FindLibrary() would return "staticLibB" if this test program:
+// - does _not_ link sharedLibA.
+// - links sharedLibB _before_ sharedLibA.
+//
 TEST_CASE( "staticLibrarySymbolCollision" )
 {
     CHECK( A::FindLibrary() == "staticLibA" );
