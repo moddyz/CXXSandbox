a3baf984aea76a77f74619e55d9a9ee984b758ef
moddyz <moddy3d@gmail.com>
2020-05-11 22:03:33 +1200

    Upgrade to new CMakeTemplate macros.

--- cmake/macros/Private.cmake
+++ cmake/macros/Private.cmake
@@ -1,16 +1,17 @@
 #
-# Private utilities
+# Private build utilities.
 #
 
-# Utility for setting compilation flags.
+# Utility for setting common compilation properties, along with include paths.
 function(
-    _set_compiler_flags
+    _set_compile_properties
     TARGET_NAME
 )
     set(options)
     set(oneValueArgs)
     set(multiValueArgs
         INCLUDE_PATHS
+        DEFINES
     )
 
     cmake_parse_arguments(
@@ -21,39 +22,44 @@ function(
         ${ARGN}
     )
 
-    target_compile_options(
-        ${TARGET_NAME}
-        PRIVATE -g
-                -O3
-                -Wno-deprecated
-                -Werror
+    target_compile_options(${TARGET_NAME}
+        PRIVATE -g      # Include debug symbols.
+                -O3     # Highest degree of code optimisation.
+                -Wall   # Enable _all_ warnings.
+                -Werror # Error on compilation for warnings.
     )
 
-    target_compile_features(
-        ${TARGET_NAME}
+    target_compile_definitions(${TARGET_NAME}
+        PRIVATE
+            ${args_DEFINES}
+    )
+
+    target_compile_features(${TARGET_NAME}
         PRIVATE cxx_std_17
     )
 
-    target_include_directories(
-        ${TARGET_NAME}
-        PRIVATE $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/include>
-                ${args_INCLUDE_PATHS}
+    # Set-up include paths.
+    target_include_directories(${TARGET_NAME}
+        PUBLIC
+            $<INSTALL_INTERFACE:include>
+        PRIVATE
+            $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/include>
+            ${args_INCLUDE_PATHS}
     )
 
-endfunction() # _set_compiler_flags
+endfunction() # _set_compile_properties
 
 function(
-    _set_link_flags
+    _set_link_properties
     TARGET_NAME
 )
     target_link_options(
         ${TARGET_NAME}
         PRIVATE
-            -Wl,--no-undefined
-            -Wl,--no-as-needed
+            -Wl,--no-undefined # Link error if there are undefined symbol(s) in output object.
+            -Wl,--no-as-needed # Link to library even if it doesn't
     )
-endfunction() # _set_compiler_flags
-
+endfunction() # _set_link_properties
 
 # Utility function for deploying public headers.
 function(
@@ -78,53 +84,10 @@ function(
         COPY ${args_PUBLIC_HEADERS}
         DESTINATION ${CMAKE_BINARY_DIR}/include/${LIBRARY_NAME}
     )
+
     install(
         FILES ${args_PUBLIC_HEADERS}
         DESTINATION ${CMAKE_INSTALL_PREFIX}/include/${LIBRARY_NAME}
     )
 
 endfunction() # _install_public_headers
-
-function(
-    _finalize_cpp_library
-    LIBRARY_NAME
-)
-    set(options)
-    set(oneValueArgs)
-    set(multiValueArgs
-        INCLUDE_PATHS
-        LIBRARIES
-    )
-
-    cmake_parse_arguments(
-        args
-        "${options}"
-        "${oneValueArgs}"
-        "${multiValueArgs}"
-        ${ARGN}
-    )
-
-    # Apply common compiler flags, and include path flags.
-    _set_compiler_flags(
-        ${LIBRARY_NAME}
-        INCLUDE_PATHS
-        ${args_INCLUDE_PATHS}
-    )
-
-    _set_link_flags(
-        ${LIBRARY_NAME}
-    )
-
-    # Link to libraries.
-    target_link_libraries(
-        ${LIBRARY_NAME}
-        PRIVATE ${args_LIBRARIES}
-    )
-
-    # Install the built library.
-    install(
-        TARGETS ${LIBRARY_NAME}
-        LIBRARY DESTINATION lib
-        ARCHIVE DESTINATION lib
-    )
-endfunction() # _cpp_library_postlude
