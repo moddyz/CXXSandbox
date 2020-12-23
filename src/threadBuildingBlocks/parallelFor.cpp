41682b70edd924fffceeb2648340c2d04a05473f
moddyz <moddy3d@gmail.com>
2020-12-23 17:49:19 +1300

    Completed parallelFor example with IncrementFunctor.

--- src/threadBuildingBlocks/parallelFor.cpp
+++ src/threadBuildingBlocks/parallelFor.cpp
@@ -15,10 +15,31 @@ SerialForIncrement(std::vector<int>& numbers)
     }
 }
 
+class IncrementFunctor
+{
+public:
+    IncrementFunctor(std::vector<int>& array)
+      : m_array(array)
+    {}
+
+    void operator()(const tbb::blocked_range<int>& range) const
+    {
+        for (size_t i = range.begin(); i != range.end(); ++i) {
+            m_array[i]++;
+        }
+    }
+
+private:
+    std::vector<int>& m_array;
+};
+
 static void
 ParallelForIncrement(std::vector<int>& numbers)
 {
     PROFILE_FUNCTION();
+
+    tbb::parallel_for(tbb::blocked_range<int>(0, numbers.size()),
+                      IncrementFunctor(numbers));
 }
 
 int
