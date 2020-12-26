48e697db4bedde661b5ac2c2242c1fe26f683bc5
moddyz <moddy3d@gmail.com>
2020-12-27 10:45:20 +1300

    Added tbb graphEmpty example.

---
+++ src/threadBuildingBlocks/graphEmpty.cpp
@@ -0,0 +1,15 @@
+// An empty graph.
+
+#include <tbb/flow_graph.h>
+
+#include "utils.h"
+
+int main( int argc, char** argv)
+{
+    PROFILE_FUNCTION();
+
+    tbb::flow::graph graph;
+    graph.wait_for_all();
+    return EXIT_SUCCESS;
+}
+
