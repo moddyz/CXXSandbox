627acd18b983bd5b1e2fd6d85078d30c0628f378
moddyz <moddy3d@gmail.com>
2020-12-27 10:51:30 +1300

    Added tbb graphFunctionNode.cpp example.

---
+++ src/threadBuildingBlocks/graphFunctionNode.cpp
@@ -0,0 +1,29 @@
+#include <tbb/flow_graph.h>
+
+#include "utils.h"
+
+int
+main(int argc, char** argv)
+{
+    PROFILE_FUNCTION();
+
+    // Construct a graph.
+    tbb::flow::graph graph;
+
+    // Construct a "squaring" function_node with 1 int input, and 1 int output.
+    tbb::flow::function_node<int, int> node(graph, 1, [](int input) -> int {
+        printf("Consumed %i input\n", input);
+        int output = input * input;
+        printf("Produced %i output\n", output);
+        return output;
+    });
+
+    node.try_put(1);
+    node.try_put(2);
+    node.try_put(3);
+
+    graph.wait_for_all();
+
+    return EXIT_SUCCESS;
+}
+
