1597b62777966d21a22837c0cec8fb40d5c50a44
moddyz <moddy3d@gmail.com>
2021-01-03 13:50:51 +1300

    Setting up containers dir for custom container implementations.

---
+++ src/containers/testVector.cpp
@@ -0,0 +1,7 @@
+#include <catch2/catch.hpp>
+
+#include "vector.h"
+
+TEST_CASE( "Vector_Initializer" )
+{
+}
---
+++ src/containers/vector.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/// \file vector.h
+///
+/// A custom implementation of a dynamically re-sizable array
+/// with templated value type.
+
+/// \class Vector
+///
+/// \tparam ValueT The type of each element.
+template< typename ValueT >
+class Vector
+{
+
+
+};
